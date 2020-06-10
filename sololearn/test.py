948794f4041f58b34c1514d7010ac831cb222c54
Anton <lowmoode92@gmail.com>
2020-06-10 08:33:57 +0300

    data hiding next

--- sololearn/test.py
+++ sololearn/test.py
@@ -741,23 +741,36 @@ import random
 # c[0] = -1 # Decrease ref. count of <42>
 
 
-class Queue:
-    def __init__(self, contents):
-        self._hiddenlist = list(contents)
+# class Queue:
+#     def __init__(self, contents):
+#         self._hiddenlist = list(contents)
+# 
+#     def push(self, value):
+#         self._hiddenlist.insert(0, value)
+# 
+#     def pop(self):
+#         return self._hiddenlist.pop(-1)
+# 
+#     def __repr__(self):
+#         return "Queue({})".format(self._hiddenlist)
+# 
+# queue = Queue([1,2,3])
+# print(queue)
+# queue.push(0)
+# print(queue)
+# queue.pop()
+# print(queue)
+# print(queue._hiddenlist)
 
-    def push(self, value):
-        self._hiddenlist.insert(0, value)
 
-    def pop(self):
-        return self._hiddenlist.pop(-1)
+# Double underscore mangled the names
 
-    def __repr__(self):
-        return "Queue({})".format(self._hiddenlist)
+class Spam:
+  __egg = 7
+  def print_egg(self):
+    print(self.__egg)
 
-queue = Queue([1,2,3])
-print(queue)
-queue.push(0)
-print(queue)
-queue.pop()
-print(queue)
-print(queue._hiddenlist)
+s = Spam()
+s.print_egg()
+print(s._Spam__egg)
+print(s.__egg)
